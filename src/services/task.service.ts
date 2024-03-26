ca6b691bab52f8d1ed9a7f5a1c3934ac455085ea
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-03-26 10:55:44 -0300

    feat: add image pulling

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,5 +1,13 @@
 import { DockerClient } from '../plugins/docker-client';
 
+interface Task {
+	image: string;
+	tag: string;
+	name: string;
+	port: number;
+	parameters?: any[];
+}
+
 export class TaskService {
 	private dockerClient: DockerClient;
 
@@ -7,7 +15,41 @@ export class TaskService {
 		this.dockerClient = new DockerClient();
 	}
 
-	public async executeTask(): Promise<string> {
-		return this.dockerClient.run();
+	public async executeTask({
+		image,
+		name,
+		tag,
+		port,
+		parameters,
+	}: Task): Promise<string> {
+		try {
+			const pulled = await this.dockerClient.pull(`${image}:${tag}`);
+
+			if (!pulled) {
+				return JSON.stringify({
+					message: 'Error pulling image',
+				});
+			}
+
+			// TODO: "Run the container with the image"
+			// const running = await this.dockerClient.run(image, tag, port);
+
+			// if (!running) {
+			// 	return JSON.stringify({
+			// 		message: 'Error running the container',
+			// 	});
+			// }
+
+			// TODO: "HTTP Request to the container to get the result of the task"
+			//? name is the name of the task we have to execute & parameters will be the parameters to pass on the http body
+
+			return JSON.stringify({
+				message: `${name} executed successfully`,
+				result: 'result',
+			});
+		} catch (error) {
+			console.error('Error executing task:', error);
+			return `Error executing task: ${error}`; // Rechazar la promesa con el error para que sea manejado por el código que llamó a executeTask()
+		}
 	}
 }
