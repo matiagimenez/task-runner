6d1b29228df25d2142bb1a36ecd4af3c8e702398
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-03-26 10:55:25 -0300

    feat: implements image pulling from dockerhub

--- src/plugins/docker-client.ts
+++ src/plugins/docker-client.ts
@@ -10,48 +10,26 @@ export class DockerClient {
 		});
 	}
 
-	public async run(): Promise<string> {
+	public async pull(image: string): Promise<boolean> {
 		return new Promise((resolve, reject) => {
-			this.docker.pull(
-				'nginx:latest',
-				(err: string, stream: Readable) => {
-					if (err) {
-						console.error('Error pulling image:', err);
-						reject('Error pulling image');
-						return;
-					}
-
-					this.docker.modem.followProgress(
-						stream,
-						() => {
-							console.log('Image pulled successfully');
-							// Una vez que la imagen se haya descargado correctamente, inicia el contenedor
-							// this.docker.createContainer(/* Configuración del contenedor */, (createErr, container) => {
-							//     if (createErr) {
-							//         console.error('Error creating container:', createErr);
-							//         reject('Error creating container');
-							//         return;
-							//     }
-							//     // Inicia el contenedor
-							//     container.start((startErr) => {
-							//         if (startErr) {
-							//             console.error('Error starting container:', startErr);
-							//             reject('Error starting container');
-							//             return;
-							//         }
-							//         // El contenedor se ha iniciado correctamente
-							//         console.log('Container running');
-							//         resolve('Container running');
-							//     });
-							// });
-						},
-						(progressErr) => {
-							console.error('Error pulling image:', progressErr);
-							reject('Error pulling image');
-						}
-					);
+			this.docker.pull(image, (err: string, stream: Readable) => {
+				if (err) {
+					console.error('Error pulling image:', err);
+					reject(false);
+					return;
 				}
-			);
+
+				this.docker.modem.followProgress(
+					stream,
+					() => {
+						console.log('Image pulled successfully');
+						resolve(true);
+					},
+					(progress) => {
+						console.log('Pulling progress:', progress);
+					}
+				);
+			});
 		});
 	}
 }
